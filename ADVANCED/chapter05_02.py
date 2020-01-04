6c74487eee19ea65bf4bd4f800e5886167d5c059
sseonmo <mo3509@gmail.com>
2020-01-04 22:25:59 +0900

    [20.1.4][python_advanced][chapter_05] Object Reference - 08. Class Structure - 5 학습 - 추상클래스 # 자체적으로 객체 생성 불가 # 상속을 통해서 자식 클래스에서  인스턴스를 생성해야함 # 개발과 관련된 공통된 내용(필드, 메소드)을 추출 및 통합해서 공통된 내용으로 작성하게 하는 것

--- ADVANCED/chapter05_02.py
+++ ADVANCED/chapter05_02.py
@@ -8,7 +8,7 @@
 #           - 변수명으로 method 생성하는 것이 관례
 # 		    - Getter : @property 어노테이션 이용함
 # 	        - Setter : @[getter 메소드명].setter / Getter의 메소드명과 동일하게 생성해야함.
-#   __slot__ 예제
+#   __slot__ 예제 - 메모리 절감효과
 #   객체슬라이딩, 인덱싱
 #   ABC, 상속, 오버라이딩
 
@@ -22,6 +22,7 @@ class VectorP(object):
 		self.__y = float(y)
 
 	def __iter__(self):
+		print('__init__ call')
 		return (i for i in (self.__x, self.__y))  # Generator
 
 	# Getter 역활
@@ -103,6 +104,7 @@ def repeat_outer(obj):
 	def repeat_inner():
 		obj.a = 'TEST'
 		del obj.a
+
 	return repeat_inner
 
 print(min(timeit.repeat(repeat_outer(use_slot), number=10000)))
@@ -129,12 +131,113 @@ print('EX3-2 -', len(s))
 print('EX3-3 -', len(s._numbers))
 print('EX3-4 -', s[1:100])
 print('EX3-5 -', s[-1])
+# 시퀀스객체[::증가폭]
 print('EX3-5 -', s[::10])
+# 시퀀스객체[시작인덱스::증가폭]
+print('EX3-6 -', s[::5])
 
 # 파이썬 추상클래스
 # 참고 : https://docs.python.org/3/library/collections.abc.html
 
 # 추상클래스 사용이유
 # 자체적으로 객체 생성 불가
-# 상속을 통해서 자식 클래스에서 자식클래스에서 인스턴스를 상속해야함
+# 상속을 통해서 자식 클래스에서  인스턴스를 생성해야함
 # 개발과 관련된 공통된 내용(필드, 메소드)을 추출 및 통합해서 공통된 내용으로 작성하게 하는 것
+
+# Sequence 상속 받지 않았지만, 자동으로 __iter__, __contailn__ 기능 작동
+# 객체 전체를 자동으로조사 -> 시퀀스 프로토콜
+
+class IterTestA:
+	def __getitem__(self, item):
+		print(repr(item))
+		return range(1, 50, 2)[item]  # range(1, 50, 2)
+
+i1 = IterTestA()
+print('EX4-1 -', i1[4])
+print('EX4-2 -', i1[4:10])
+print('EX4-3 -', 3 in i1[1:10])
+# print('EX4-4 -', [i for i in i1])     # 이해가 안됨
+# print('EX4-4 -', [i for i in i1[:]])
+
+print()
+print()
+
+# Sequence 상속
+# 요구사항인 추상메소드를 모두 구현해야 동작
+
+from collections.abc import Sequence
+
+class IterTestB(Sequence):
+	def __getitem__(self, item):
+		print('__getitem__', repr(item))
+		return range(1, 50, 2)[item]  # range(1, 50, 2)
+
+	def __len__(self, idx):
+		print('__len__', repr(idx))
+		return len(range(1, 50, 2)[idx])
+
+i2 = IterTestB()
+print('EX4-5 -', i2[4])
+print('EX4-6 -', i2[4:10])
+print('EX4-7 -', 3 in i2[1:10])
+print()
+print()
+# abc 활용예제 - abstract class
+import abc
+
+class RandomMachine(abc.ABC):  # metaclass=abc.ABCMeta(3.4이하)
+	# __metaclass__ = abc.ABCMeta
+
+	# 추상메소드
+	@abc.abstractmethod
+	def load(self, iterobj):
+		"""Iterable 항목추가"""
+
+	# 추상메소드
+	@abc.abstractmethod
+	def pick(self):
+		"""무작위 항목 뽑기"""
+
+	def inspect(self):
+		items = []
+		while True:
+			try:
+				items.append(self.pick())
+			except LookupError:
+				break
+			return tuple(sorted(items))
+
+import random
+
+class CraneMachine(RandomMachine):
+	def __init__(self, items):
+		self._readomizer = random.SystemRandom()
+		self._items = []
+		self.load(items)
+
+	def load(self, iterobj):
+		self._items.extend(iterobj)
+		self._readomizer.shuffle(self._items)
+
+	def pick(self):
+		try:
+			return self._items.pop()
+		except IndexError:  # item이 더이상 없을때 에러발생함.
+				raise LookupError('Empty Crane Box')
+
+	def __call__(self):
+		return self.pick()
+
+# 서브클래스 확인 - issubclass(자식, 부모)
+print('EX5-1 -', issubclass(RandomMachine, CraneMachine))
+print('EX5-2 -', issubclass(CraneMachine, RandomMachine))
+
+# 상속 구조 확인
+print('EX5-3 -', CraneMachine.__mro__)
+cm = CraneMachine(range(1, 100)) # 추상메소드 구현 안하면 에러
+
+print('EX5-4 -', cm._items)
+print('EX5-5 -', cm.pick())
+print('EX5-6 -', cm())
+print('EX5-7 -', cm.inspect())
+
